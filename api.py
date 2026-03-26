0d84bfc03a045d1af7e4dd9c41fe90b214a1f2f9
Gouri Titarmare <gouri.titarmare07@gmail.com>
2026-03-26 17:54:12 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -36,5 +36,7 @@ def predict():
         if os.path.exists("temp_audio"):
             os.remove("temp_audio")
 
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
